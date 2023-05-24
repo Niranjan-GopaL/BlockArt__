266a606278185b763490b14f8aa8597db86c7f86
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-05-24 17:55:05 +0530

    2 Main Section Wrappers in Landing page

--- __landing_page__/src/App.jsx
+++ __landing_page__/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
+import {Button,Download,Features,SectionWrapper} from './components'
+import assets from "./assets"
 
 const  App = () => {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 >Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <SectionWrapper 
+        title="You own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
     </>
   )
 }
