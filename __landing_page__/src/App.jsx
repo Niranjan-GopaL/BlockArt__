018b50c6fb0e51f4e5f8f7aea7085d902b1fdca1
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 23:31:41 +0530

    Download component done

--- __landing_page__/src/App.jsx
+++ __landing_page__/src/App.jsx
@@ -43,6 +43,7 @@ const App = () => {
 
             <Download />
 
+            {/* Footer component */}
             <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
                 <p className={`${styles.pText} ${styles.whiteText}`}>
                     Made with love by{" "}
--- __landing_page__/src/components/Download.jsx
+++ __landing_page__/src/components/Download.jsx
@@ -1,7 +1,31 @@
+import styles from '../styles/Global';
+import assets from '../assets';
+
 const Download = () => {
   return (
-    <div>
-      Download
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+
+
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+        </div>
+
+        {/* You can share the source code. Populate the onClick and href ppty */}
+        <button className={styles.btnPrimary} onClick={{}} href="#" >Source Code</button>
+
+        <div className={styles.flexCenter}>
+          {/* This is that badass image with 5 different mockups */}
+          <img 
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+
+
+      </div>
     </div>
   )
 }
