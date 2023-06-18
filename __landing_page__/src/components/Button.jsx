a9c81952bd23c3fae4405800817e5c3792aba041
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-06-18 22:29:49 +0530

    Button that says View in Expo store DONE

--- __landing_page__/src/components/Button.jsx
+++ __landing_page__/src/components/Button.jsx
@@ -1,8 +1,21 @@
+// the button that shows "VIEW IT ON EXPO STORE"
+import styles from '../styles/Global';
 
-const Button = () => {
+const Button = ({ assetUrl, link }) => {
   return (
-    <div>
-      Button
+    <div 
+      className={styles.btnBlack}
+
+      // Since we want to open it in a new tab we pass in the "_blank" prop 
+      onClick={() => window.open(link, "_blank")}
+    >
+      {/* expo icon as said in alt lolll */}
+      <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+
+      <div className="flex flex-col justify-start ml-4">
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+      </div>
     </div>
   )
 }
